e84fafac8decb5bd8cf2dd42a8aed48a4de228ea
jongro juna <wnsghk6670@naver.com>
2021-05-24 17:29:58 +0900

    웹프로그래밍 기초 파일명 수정 및 프라이머리키를 사용하기 위한 변수 설정

--- src/member/MemberServlet.java
+++ src/member/MemberServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import kr.jobtc.json.DBConn;
 
 @WebServlet(urlPatterns = "/member/member.do")
-public class MemberServelt extends HttpServlet {
+public class MemberServlet extends HttpServlet {
 	Connection conn;	//데이터베이스와 연결하기위해 선언한 Connection 타입의 참조변수
 	MemberDao dao;	//MemberDao 의 기능들을 사용하기 위해 미리 참조변수를 선언함.
 	RequestDispatcher disp; //요청결과를 forward 할때 사용됨.
@@ -33,14 +33,27 @@ public class MemberServelt extends HttpServlet {
 		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
 		//한글로 입력시 깨짐을 방지하기 위해 설정해 두었다.
+		MemberVo mVo = null;
+		String mId = "";
+		String pwd = "";
 		
 		String url = "/member/";	//공통 경로를 변수에 담아뒀다.
 		dao = new MemberDao(conn);	// conn 객체를 매개변수로 받아 MemberDao 객체를 생성하고 참조변수에 담는다.
 		Page page = new Page();	//Page 객체를 생성하여 해당 타입의 참조변수에 그 주소값을 담는다
-		
+		String tempNowPage = req.getParameter("nowPage");
 		
 		if(req.getParameter("job") != null) {	//파라메터의 값이 null 이 아닐경우
 			job = req.getParameter("job");	//해당 파라메터 값을 job에 저장해둔다.
+		}
+		
+		if(req.getParameter("findStr") != null) {
+			page.setFindStr(req.getParameter("findStr"));	//findStr의 값이 null 이 아닐시 그 값을 파라메터로 가져와 page의 findStr로 설정하여 준다.
+		}
+		
+		if(tempNowPage == null || tempNowPage.equals("")) {
+			page.setNowPage(1);
+		}else {
+			page.setNowPage(Integer.parseInt(tempNowPage));
 		}
 		
 		switch(job) {	//job 이라는 파라메터가 각각의 경우일때 해당 url에 파일.jsp를 추가하여 페이지를 불러올 수 있도록 세팅
@@ -50,17 +63,42 @@ public class MemberServelt extends HttpServlet {
 			req.setAttribute("list", list);	//req 객체의 list 속성을 list로 설정하여줌
 			req.setAttribute("page", page);
 			break;
+			
 		case "modify" :
 			url += "modify.jsp";
+			mId = req.getParameter("mid");
+			mVo = dao.view(mId);
+			req.setAttribute("vo", mVo);
+			
+			
 			break;
+			
 		case "modifyR" :
 			url += "modify_result.jsp";
 			break;
+			
 		case "deleteR" :
-			url += "delete_result.jsp";
+			url += "search.jsp";
+			
+			mId = req.getParameter("mid");
+			pwd = req.getParameter("pwd");
+			mVo = new MemberVo();
+			mVo.setMid(mId);
+			mVo.setPwd(pwd);
+			
+			dao.delete(mVo);
+			
+			list = dao.select(page);
+			req.setAttribute("page", page);
+			req.setAttribute("list", list);
+			
 			break;
 		case "view" :
 			url += "view.jsp";
+			mId = req.getParameter("mid");
+			mVo = dao.view(mId);
+			req.setAttribute("vo", mVo);
+			
 			break;
 		case "register" :
 			url += "register.jsp";
