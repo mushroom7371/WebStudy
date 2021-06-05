5cd8694d2f58e529c3a694123aea1c65adc6f412
juna desktop <wnsghk6670@naver.com>
2021-06-05 18:32:19 +0900

    파일 업로드 dao 작성

--- src/board/BoardDao.java
+++ src/board/BoardDao.java
@@ -79,17 +79,51 @@ public class BoardDao {
 		return msg;
 	}
 	
-	public String update(BoardVo vo){
-		String msg = "ok..";
+	public String update(ReViewVo vo) {
+		String msg = "수정되었습니다.";
+		int r = 0;
+		int chkCnt = 0;
+		
 		try {
-			
+			r = sqlSession.update("review.rev_update", vo);
+			sqlSession.commit();
+
+			if(r < 1) throw new Exception();
+			chkCnt = 0;
+			if(vo.getAttList() != null) {
+				for(ReViewAttVo v : vo.getAttList()) {
+					System.out.println("chkcnt " +chkCnt);
+					v.setpSerial(vo.getSerial());
+					chkCnt += sqlSession.insert("review.revAtt_insert", v);
+					sqlSession.commit();
+				}
+				
+				if(chkCnt == vo.getAttList().size()) {
+					for(ReViewAttVo delVo : vo.getDelList()) {
+						sqlSession.delete("review.revAtt_delete2", delVo.getSysAtt());
+						sqlSession.commit();
+						File f = new File(ReViewFileUpload.saveDir + delVo.getSysAtt());
+						if(f.exists()) f.delete();
+					}
+					sqlSession.close();
+				}else {
+					throw new Exception();
+				}
+			} //첨부파일 유무로 수정
 		}catch(Exception ex) {
+			ex.printStackTrace();
+			System.out.println("r=" + r);
+			System.out.println("chkCnt=" + chkCnt);
 			msg = ex.toString();
 			ex.printStackTrace();
-		}finally {
-			sqlSession.close();
-			return msg;
+			sqlSession.rollback();
+			for(ReViewAttVo delVo : vo.getAttList()) {
+				File f = new File(ReViewFileUpload.saveDir + delVo.getSysAtt());
+				if(f.exists()) f.delete();
+			}
 		}
+		sqlSession.close();
+		return msg;
 	}
 	
 	public String repl(BoardVo vo){
